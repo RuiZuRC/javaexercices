d0df8b4b2f2000944e63168be8197ceb4c90fd54
RuiZuRC <ruicrux@hotmail.com>
2019-11-04 21:22:12 +0000

    Rectangulo, codigo final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Hello World");
+
+        Rectangle r2 = new Rectangle(1.1f,1.1f);
+        System.out.println(r2.toString());
     }
 }
