947d294aa785832cbfde91262af08387e5098f49
RuiZuRC <ruicrux@hotmail.com>
2019-11-04 21:09:07 +0000

    Rectangulo

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    private float length = 1.0f;
+    private float width = 1.0f;
+}
+
+public Rectangle(){}
+
+public Rectangle(float length, float width){
+    this.length = length;
+    this.width = width;
+}
+
